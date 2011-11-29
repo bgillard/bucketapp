4892c4a52e34a5b92dcf0ee869f1844b49c9c57e
Bradley E. Gillard <bradleygillard@gmail.com>
2011-11-28 21:14:06 -0600

    First commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bucketapp::Application.config.secret_token = 'c961c4de2bf1499ed32a67af072892a996ecb29e80b2a25b08a611db4d923f9d731592e163eb34c93c9a3102e3219593059f4b416c0c44b974760c4fa404fd00'
